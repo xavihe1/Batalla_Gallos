ed3ac6f88539bdc59c0b27ec5eaa5d47d08a16bf
arnau <arnau.casas.7e7@itb.cat>
2024-05-27 08:56:09 +0200

    sa

--- src/main/kotlin/com/example/batalla_gallos/HelloApplication.kt
+++ src/main/kotlin/com/example/batalla_gallos/HelloApplication.kt
@@ -9,7 +9,7 @@ class HelloApplication : Application() {
     override fun start(stage: Stage) {
         val fxmlLoader = FXMLLoader(HelloApplication::class.java.getResource("menu.fxml"))
         val scene = Scene(fxmlLoader.load(), 320.0, 240.0)
-        stage.title = "Hello!"
+        stage.title = "Epicas batallas de rap!"
         stage.scene = scene
         stage.show()
     }
